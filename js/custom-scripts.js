78680ba6092d4f684f7e38e51d65bbb8875f3967
Iegor Stetsiura <biropka@gmail.com>
2016-08-06 01:39:35 +0300

    v 0.1

--- js/custom-scripts.js
+++ js/custom-scripts.js
@@ -1,1 +1,20 @@
+$(document).ready(function () {
 
+    /******************************
+     ********* main page ***********
+     ******************************/
+
+    $('.custom-select .current-value').click(function() {
+        $(this).toggleClass('active');
+        $(this).next().slideToggle();
+    });
+
+    $('.form-order .custom-select ul li').click(function() {
+        $(this).parent().slideUp();
+        $(this).parent().siblings('.current-value').removeClass('active');
+        var value = $(this).html();
+        $(this).parent().siblings('.current-value').html(value);
+        $(this).parent().next().find('input').val(value);
+    });
+
+});
