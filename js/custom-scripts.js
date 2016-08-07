8ffe6dc2182670901b5ed7ad541f3d9b536386df
Iegor Stetsiura <biropka@gmail.com>
2016-08-07 19:09:56 +0300

    v 0.1

--- js/custom-scripts.js
+++ js/custom-scripts.js
@@ -1,6 +1,23 @@
 $(document).ready(function () {
 
     /******************************
+     ******* init scripts *********
+     ******************************/
+    if ($(window).width() > '1024'){
+        $('.header-contact').css('display', 'block')
+    } else {
+        $('.header-contact').css('display', 'none')
+    }
+
+    $(window).resize(function(){
+        if ($(window).width() > '1024'){
+            $('.header-contact').css('display', 'block')
+        } else {
+            $('.header-contact').css('display', 'none')
+        }
+    });
+
+    /******************************
      ********* main page ***********
      ******************************/
 
@@ -15,6 +32,16 @@ $(document).ready(function () {
         var value = $(this).html();
         $(this).parent().siblings('.current-value').html(value);
         $(this).parent().next().find('input').val(value);
+    });
+
+    $('.mobile-contacts').click(function() {
+        $(this).toggleClass('active');
+        $('.header-contact').fadeToggle(200);
+    });
+
+    $('.mobile-menu').click(function() {
+        $(this).toggleClass('active');
+        $('nav ul').fadeToggle(200);
     });
 
 });
