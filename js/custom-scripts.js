dc899ef81486d57bf5c1c9fbbe0a24a46e644554
bIropka <biropka@gmail.com>
2016-08-08 15:51:53 +0300

    final

--- js/custom-scripts.js
+++ js/custom-scripts.js
@@ -52,4 +52,47 @@ $(document).ready(function () {
         $('.user-phone').mask("+7 ( 9 9 9 ) 9 9 9 - 9 9 - 9 9");
     });
 
+    var show = true;
+    var countbox = ".statistics";
+    $(window).on("scroll load resize", function() {
+
+        if (!show) return false;
+
+        var w_top = $(window).scrollTop();
+        var e_top = $(countbox).offset().top;
+
+        var w_height = $(window).height();
+        var d_height = $(document).height();
+
+        var e_height = $(countbox).outerHeight();
+
+        if (w_top + w_height * 0.8 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
+
+            var space_separator_number_step = $.animateNumber.numberStepFactories.separator(' ');
+
+            $('#number-cars').animateNumber(
+                {
+                    number: 8000,
+                    numberStep: space_separator_number_step
+                }, 1500
+            );
+
+            $('#number-years').animateNumber(
+                {
+                    number: 10,
+                    numberStep: space_separator_number_step
+                }, 1000
+            );
+
+            $('#number-money').animateNumber(
+                {
+                    number: 499520000,
+                    numberStep: space_separator_number_step
+                }, 2000
+            );
+
+            show = false;
+        }
+    });
+
 });
